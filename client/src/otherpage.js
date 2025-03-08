a1fad6c953eecaac12de2f26f96817ec4a9a6c9d
Syed Abdul Rahman <abdulrahman9205@gmail.com>
2025-03-08 14:00:55 +0800

    Completed Writing Production Docker Files

--- client/src/otherpage.js
+++ client/src/otherpage.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default () => {
+const OtherPage = () => {
   return (
     <div>
-      Some Other Page
-      <Link to="/">Go Back Home</Link>
+      Im some other page!
+      <Link to="/">Go back home</Link>
     </div>
   );
 };
+
+export default OtherPage;
